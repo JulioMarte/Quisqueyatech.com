74ee9a3b253285c656bcd78ae6ce18f12c645a4a
JulioMarte <julio19955@hotmail.com>
2026-08-13 23:17:50 -0400

    refactor: enable React components in Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
 export default defineConfig({
   site: process.env.PUBLIC_SITE_URL || "https://www.quisqueyatech.com",
   output: "static",
   trailingSlash: "never",
+  integrations: [react()],
   server: {
     port: 4221,
   },
